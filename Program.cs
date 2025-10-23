a815aa5816ea406dcdadc179b84e32bb42d960f5
Kamil Tulik <huzar01x@gmail.com>
2025-10-23 12:07:32 -0400

    Updated program.cs

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ app.MapGet("/", () => Results.Content(@"
         <head><title>Simple ASP.NET 8 App</title></head>
         <body style='font-family:Arial;text-align:center;margin-top:50px;'>
             <h1>Hello from .NET 8 on Azure App Service!</h1>
+            <h3>In the magical C we see you h3</h3>
             <p>This is a simple test deployment.</p>
         </body>
     </html>
