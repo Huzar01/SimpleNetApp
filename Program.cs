c89f12e6c925fa34d3ed7857812d6e2a41010307
Kamil Tulik <huzar01x@gmail.com>
2025-10-23 12:55:45 -0400

    Testing deploymnet slot staging

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ app.MapGet("/", () => Results.Content(@"
         <body style='font-family:Arial;text-align:center;margin-top:50px;'>
             <h1>Hello from .NET 8 on Azure App Service!</h1>
             <h3>In the magical C we see you h3</h3>
+            <br><br><br>
+            <h4> This is staging </h4> 
             <p>This is a simple test deployment.</p>
         </body>
     </html>
